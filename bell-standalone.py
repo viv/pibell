02161f6803ed2b9674420e7663800417d043431e
Matthew Vivian <matthew@postino.co.uk>
2014-05-17 20:02:25 +0100

    Added Raspberry Pi specifics and moved original to new file

--- bell-standalone.py
+++ bell-standalone.py
@@ -3,11 +3,7 @@
 from time import sleep
 import subprocess
 import httplib, urllib
-import RPi.GPIO as GPIO
 import config
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(config.bell_pin, GPIO.IN)
 
 def notifyPhones(message):
     conn = httplib.HTTPSConnection("api.pushover.net:443")
@@ -26,7 +22,11 @@ notifyPhones('Listener started')
 print 'Doorbell listener Started\r'
 
 while True:
-    if (GPIO.input(23) == False):
+    key = raw_input("Hit a key, press Q to quit: ")
+    if key.lower() == "q":
+        break
+    else:
+        print 'Button Pushed!\r'
         subprocess.Popen(["ogg123","-q","dingdong.ogg"])
         notifyPhones(config.message_text)
         sleep(3);
